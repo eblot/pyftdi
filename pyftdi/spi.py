76b172375290ce79ec0d862ec2103f392e837904
Emmanuel Blot <eblot@neotion.com>
2011-03-23 15:44:37 +0100

    Tell the FTDI device to return read data as soon as possible

--- pyftdi/spi.py
+++ pyftdi/spi.py
@@ -83,6 +83,7 @@ class SpiController(object):
                                     self._cs_bits,
                                     self._direction) + \
                         struct.pack('<BBB', Ftdi.WRITE_BITS_TMS_NVE, 7, 0xff)
+        self._immediate = struct.pack('<B', Ftdi.SEND_IMMEDIATE)
         self._frequency = 0.0
 
     def configure(self, vendor, product, interface, frequency=6.0E6):
@@ -135,12 +136,13 @@ class SpiController(object):
         write_cmd = struct.pack('<BH', Ftdi.WRITE_BYTES_NVE_MSB, len(out)-1)
         if readlen:
             read_cmd = struct.pack('<BH', Ftdi.READ_BYTES_NVE_MSB, readlen-1)
-            cmd = cs_cmd + write_cmd + out + read_cmd + self._cs_high
+            cmd = cs_cmd + write_cmd + out + read_cmd + self._immediate + \
+                  self._cs_high
             self._ftdi.write_data(cmd)
             # USB read cycle may occur before the FTDI device has actually
             # sent the data, so try to read more than once if no data is
             # actually received
-            data = self._ftdi.read_data_bytes(readlen, 2)
+            data = self._ftdi.read_data_bytes(readlen, 4)
         else:
             cmd = cs_cmd + write_cmd + out + self._cs_high
             self._ftdi.write_data(cmd)
@@ -149,5 +151,5 @@ class SpiController(object):
 
     def _flush(self):
         """Flush the HW FIFOs"""
-        self._ftdi.write_data(struct.pack('<B', Ftdi.SEND_IMMEDIATE))
+        self._ftdi.write_data(self._immediate)
         self._ftdi.purge_buffers()
