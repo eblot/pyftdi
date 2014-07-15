d60ae345b70395ec4a6841bb7c58c6b0d37cdf69
Emmanuel Blot <emmanuel.blot@free.fr>
2014-07-15 16:37:05 +0200

    Update version number

--- pyftdi/__init__.py
+++ pyftdi/__init__.py
@@ -23,5 +23,5 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-version_info = (0, 9, 4)
+version_info = (0, 9, 5)
 __version__ = '.'.join(['%d' % v for v in version_info])
