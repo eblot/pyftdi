3e3ea6574cefbe1e87f7e17a2bb25fe13cb9915f
Emmanuel Blot <emmanuel.blot@free.fr>
2019-06-11 11:15:51 +0200

    No need to mess with Apple kext on macOS 10.14+

--- pyftdi/bin/uphy.sh
+++ pyftdi/bin/uphy.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# Load/unload kernel extension helper for macOS
+
+system=$(uname -s)
+if [ "${system}" != "Darwin" ]; then
+    echo "This script is dedicated to macOS" >&2
+    exit 1
+fi
+version=$(sw_vers -productVersion | cut -d. -f2)
+if [ ${version} -lt 9 ]; then
+    echo "This version of OS X does not use an Apple FTDI driver"
+    exit 0
+fi
+if [ ${version} -gt 13 ]; then
+    echo "Apple FTDI driver on this macOS version should not be unloaded" >&2
+    exit 1
+fi
 kextstat 2>/dev/null | grep com.apple.driver.AppleUSBFTDI > /dev/null
 if [ $? -eq 0 ]; then
     echo "Admin priviledge required to unload Apple FTDI driver"
