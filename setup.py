6adc09611dd950a29325a495ebf2a7baa90dc3f2
Emmanuel Blot <emmanuel.blot@free.fr>
2020-03-13 08:45:08 +0100

    Work around distutils mess distutils package invalid files (that do not even compile) w/o any issues...

--- setup.py
+++ setup.py
@@ -27,15 +27,21 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import codecs
-import os
-import re
-
+from codecs import open as codec_open
+from os import close, unlink
+from os.path import abspath, dirname, join as joinpath
+from py_compile import compile as pycompile, PyCompileError
+from re import split as resplit, search as research
+from tempfile import mkstemp
 from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py
+
+#pylint: disable-msg=unused-variable
+
 
 NAME = 'pyftdi'
 PACKAGES = find_packages(where='.')
-META_PATH = os.path.join('pyftdi', '__init__.py')
+META_PATH = joinpath('pyftdi', '__init__.py')
 KEYWORDS = ['driver', 'ftdi', 'usb', 'serial', 'spi', 'i2c', 'twi', 'rs232',
             'gpio', 'bit-bang']
 CLASSIFIERS = [
@@ -57,9 +63,7 @@ INSTALL_REQUIRES = [
     'pyusb >= 1.0.0',
     'pyserial >= 3.0',
 ]
-
-
-HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = abspath(dirname(__file__))
 
 
 def read(*parts):
@@ -67,7 +71,7 @@ def read(*parts):
     Build an absolute path from *parts* and and return the contents of the
     resulting file.  Assume UTF-8 encoding.
     """
-    with codecs.open(os.path.join(HERE, *parts), 'rb', 'utf-8') as dfp:
+    with codec_open(joinpath(HERE, *parts), 'rb', 'utf-8') as dfp:
         return dfp.read()
 
 
@@ -75,7 +79,7 @@ def read_desc(*parts):
     """Read and filter long description
     """
     text = read(*parts)
-    text = re.split(r'\.\.\sEOT', text)[0]
+    text = resplit(r'\.\.\sEOT', text)[0]
     return text
 
 
@@ -86,17 +90,54 @@ def find_meta(meta):
     """
     Extract __*meta*__ from META_FILE.
     """
-    meta_match = re.search(
-        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
-        META_FILE, re.M
+    meta_match = research(
+        r"(?m)^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE
     )
     if meta_match:
         return meta_match.group(1)
     raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
 
 
+class BuildPy(build_py):
+    """Override byte-compile sequence to catch any syntax error issue.
+
+       For some reason, distutils' byte-compile when it forks a sub-process
+       to byte-compile a .py file into a .pyc does NOT check the success of
+       the compilation. Therefore, any syntax error is explictly ignored,
+       and no output file is generated. This ends up generating an incomplete
+       package w/ a nevertheless successfull setup.py execution.
+
+       Here, each Python file is build before invoking distutils, so that any
+       syntax error is catched, raised and setup.py actually fails should this
+       event arise.
+
+       This step is critical to check that an unsupported syntax (for ex. 3.6
+       syntax w/ a 3.5 interpreter) does not end into a 'valid' package from
+       setuptools perspective...
+    """
+
+    def byte_compile(self, files):
+        for file in files:
+            if not file.endswith('.py'):
+                continue
+            pfd, pyc = mkstemp('.pyc')
+            close(pfd)
+            try:
+                pycompile(file, pyc, doraise=True)
+                continue
+            except PyCompileError as exc:
+                # avoid chaining exceptions
+                pass
+            finally:
+                unlink(pyc)
+            raise SyntaxError("Cannot byte-compile '%s'" % file)
+        super().byte_compile(files)
+
+
 if __name__ == '__main__':
     setup(
+        cmdclass={'build_py': BuildPy},
         name=NAME,
         description=find_meta('description'),
         license=find_meta('license'),
