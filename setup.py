b366832c5371b3b8849c3247d23116240c1b203f
Emmanuel Blot <emmanuel.blot@free.fr>
2021-01-12 22:37:20 +0100

    Python 3.9 is supported

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: System :: Hardware :: Hardware Drivers',
 ]
