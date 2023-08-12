35e5bb2da0a6b3c694de998069ee63e77743826e
Emmanuel Blot <emmanuel.blot@free.fr>
2023-08-12 18:12:24 +0200

    Revert "Merge pull request #308 from Kalofin/fix-eeprom-strings"
    This reverts commit f2f619c194d53f1d7b48b63f341d91efe3bb10a6, reversing
    changes made to 9914488bde2f92dba293f3e626734f7c67b51217.

--- pyftdi/eeprom.py
+++ pyftdi/eeprom.py
@@ -723,7 +723,7 @@ class FtdiEeprom:
             self._eeprom[s1_vstr_start:s1_vstr_start+len(stream)] = stream
         self._eeprom[dynpos:dynpos+len(stream)] = stream
         mtp = self._ftdi.device_version == 0x1000
-        crc_pos = ( 0x100 if mtp else self._size ) - 2
+        crc_pos = 0x100 if mtp else self._size
         rem = crc_pos - (dynpos + len(stream))
         if rem < 0:
             oversize = (-rem + 2) // 2
