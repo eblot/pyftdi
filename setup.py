93954e248335004daa8b22297feb1224c3f59df4
Emmanuel Blot <emmanuel.blot@free.fr>
2024-04-06 17:35:39 +0200

    add Python 3.12

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: System :: Hardware :: Hardware Drivers',
 ]
