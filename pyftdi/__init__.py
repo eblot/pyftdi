5ad236905526b81cf46cdf3bba42f6bad00f557f
Emmanuel Blot <emmanuel.blot@free.fr>
2024-05-01 17:32:32 +0200

    version: prepare from API breakage: v0.60 API is no longer compatible
    Signed-off-by: Emmanuel Blot <emmanuel.blot@free.fr>

--- pyftdi/__init__.py
+++ pyftdi/__init__.py
@@ -6,7 +6,7 @@
 
 # pylint: disable=missing-docstring
 
-__version__ = '0.56.0'
+__version__ = '0.59.1'
 __title__ = 'PyFtdi'
 __description__ = 'FTDI device driver (pure Python)'
 __uri__ = 'http://github.com/eblot/pyftdi'
